41f966471668a0715d8af025f95a80249d661470
ShAwn <shawnsthottan@gmail.com>
2017-06-17 19:53:15 +0530

    orientation

--- app/src/main/java/com/example/android/managers/EmergenciesAdapter.java
+++ app/src/main/java/com/example/android/managers/EmergenciesAdapter.java
@@ -2,17 +2,14 @@ package com.example.android.managers;
 
 import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
+import android.graphics.Color;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.List;
-
-import static java.security.AccessController.getContext;
 
 /**
  * Created by RoshanJoy on 20-03-2017.
@@ -36,18 +33,54 @@ public class EmergenciesAdapter  extends ArrayAdapter<Emergencies>{
         TextView mUsername = (TextView)convertView.findViewById(R.id.EmergenciesUsername);
         TextView mseverity = (TextView)convertView.findViewById(R.id.EmergenciesSeverity);
         TextView mtype = (TextView)convertView.findViewById(R.id.EmergenciesType);
-
+        View v = (View) convertView.findViewById(R.id.view1);
 
         final Emergencies emergencies = getItem(position);
 
+        String string;
+        string="low";
+        v.setBackgroundColor(Color.parseColor("green"));
+
+        if(emergencies.emergencyDetails.getSi().equals("2"))
+        {
+            string="medium";
+            v.setBackgroundColor(Color.parseColor("#ffa500"));
+        }
+        else if(emergencies.emergencyDetails.getSi().equals("3"))
+        {
+            string="high";
+            v.setBackgroundColor(Color.parseColor("red"));
+        }
+
+        String string1="fire";
+        if(emergencies.emergencyDetails.getTi().equals("2"))
+        {
+            string1="pregnancy";
+        }
+        if(emergencies.emergencyDetails.getTi().equals("3"))
+        {
+            string1="heart";
+        }
+        if(emergencies.emergencyDetails.getTi().equals("4"))
+        {
+            string1="accident";
+        }
+        if(emergencies.emergencyDetails.getTi().equals("5"))
+        {
+            string1="head injury";
+        }
+        if(emergencies.emergencyDetails.getTi().equals("6"))
+        {
+            string1="other";
+        }
 
         mName.setText(""+emergencies.emergencyDetails.getUsername());
         Log.v("emer","");
         mUsername.setText(""+emergencies.emergencyDetails.getUsername());
         Log.v("emer","");
-        mseverity.setText(""+emergencies.emergencyDetails.getSi());
+        mseverity.setText(""+string);
         Log.v("emer","");
-        mtype.setText(""+emergencies.emergencyDetails.getTi());
+        mtype.setText(""+string1);
         Log.v("emer","");
 
 
